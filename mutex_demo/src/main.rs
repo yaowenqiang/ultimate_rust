c9ca9e26f52f3c4e1e99b792132ad2d89aead5a5
yaowenqiang <yaowenqiang111@163.com>
2025-09-20 20:14:58 +0800

    add mutex demo

---
+++ mutex_demo/src/main.rs
@@ -0,0 +1,18 @@
+use std::sync::Mutex;
+
+static NUMBERS: Mutex<Vec<u32>> = Mutex::new(Vec::new());
+
+fn main() {
+    let mut handles = Vec::new();
+    for _ in 0..10 {
+        let handle = std::thread::spawn(|| {
+            let mut lock = NUMBERS.lock().unwrap();
+            lock.push(1);
+        });
+        handles.push(handle);
+    }
+    handles.into_iter().for_each(|h| h.join().unwrap());
+
+    let lock = NUMBERS.lock().unwrap();
+    println!("{:#?}", lock);
+}
