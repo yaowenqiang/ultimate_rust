4633bcbb576b3c3ebdd75a31cf1d83aee34f33b9
yaowenqiang <yaowenqiang111@163.com>
2025-09-20 11:11:29 +0800

    add get thread returns demo

--- threads_demo/src/main.rs
+++ threads_demo/src/main.rs
@@ -2,13 +2,23 @@ use std::thread;
 fn hello_thread(n: u32) {
     println!("Hello from thread! {n}");
 }
+
+fn do_math(i: u32) -> u32 {
+    let mut n = i + 1;
+    for _ in 0..10 {
+        n *= 2;
+    }
+    n
+}
 fn main() {
     println!("Hello from main thread!");
     let mut thread_handles = Vec::new();
     for i in 0..10 {
-        let thread_handle = thread::spawn(move || hello_thread(i));
+        let thread_handle = thread::spawn(move || do_math(i));
         thread_handles.push(thread_handle);
     }
 
-    thread_handles.into_iter().for_each(|h| h.join().unwrap());
+    thread_handles
+        .into_iter()
+        .for_each(|h| println!("{}", h.join().unwrap()));
 }
