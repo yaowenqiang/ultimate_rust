0dba8058f5c934c81f5a4ca0c90eea953b03ff08
yaowenqiang <yaowenqiang111@163.com>
2025-09-26 23:54:58 +0800

    add tokio pin demo

--- recursion/src/main.rs
+++ recursion/src/main.rs
@@ -1,4 +1,5 @@
 use async_recursion::*;
+use std::pin::Pin;
 #[async_recursion]
 async fn fibonacci(n: u32) -> u32 {
     match n {
@@ -8,7 +9,31 @@ async fn fibonacci(n: u32) -> u32 {
     }
 }
 
+async fn one() {
+    println!("one");
+}
+
+async fn two() {
+    println!("two");
+}
+
+async fn call_one_or_two(n: u32) -> Pin<Box<dyn Future<Output = ()>>> {
+    match n {
+        1 => Box::pin(one()),
+        2 => Box::pin(two()),
+        _ => panic!("Invalid choice"),
+    }
+}
+
 #[tokio::main]
 async fn main() {
     println!("fibonacci(10): {}", fibonacci(10).await);
+
+    let mut future = async {
+        println!("Hello world");
+    };
+    tokio::pin!(future);
+    (&mut future).await;
+    call_one_or_two(1).await;
+    call_one_or_two(2).await;
 }
